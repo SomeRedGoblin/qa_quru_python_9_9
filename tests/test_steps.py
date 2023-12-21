ea846350e4280e5177c1f98bd1de1920880c2693
Andrey Rusak <dudzy@mail.ru>
2023-12-21 20:27:44 +0300

    Поправил форматирование

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -20,6 +20,7 @@ def test_dynamic_steps():
     with allure.step("Проверяем что элемент с текстом #76 виден"):
         browser.element(by.partial_text("#76")).should(be.visible)
 
+
 def test_decorator_steps():
     open_main_page()
     search_for_repository("eroshenkoam/allure-example")
@@ -27,9 +28,11 @@ def test_decorator_steps():
     open_issue_tab()
     should_see_issue_with_number("#76")
 
+
 @allure.step("Открываем главную страницу")
 def open_main_page():
     browser.open("/")
+
 
 @allure.step("Ищем репозиторий {repo}")
 def search_for_repository(repo):
@@ -37,16 +40,17 @@ def search_for_repository(repo):
     browser.element("#query-builder-test").send_keys(repo)
     browser.element("#query-builder-test").submit()
 
+
 @allure.step("Переходим по ссылке репозитория {repo}")
 def go_to_repository(repo):
     browser.element(by.link_text(repo)).click()
+
 
 @allure.step("Открываем таб Issues")
 def open_issue_tab():
     browser.element("#issues-tab").click()
 
+
 @allure.step("Проверяем наличие Issue с номером {number}")
 def should_see_issue_with_number(number):
     browser.element(by.partial_text(number)).should(be.visible)
-
-
